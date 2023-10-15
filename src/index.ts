590b8084552f652e80890fdc2fd9879872c5556f
aboli <duniandewon@gmail.com>
2023-10-15 15:53:15 +0700

    Add port env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import app from "./app"
+import app from "./app";
 
-const PORT = 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
